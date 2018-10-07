297fd0bb4965a177203c1a2e7ad954e4e0b47458
Nicolas Marzullo <marzullonicolas@gmail.com>
2018-10-07 14:42:22 -0300

    agregadp de caño izquierdo a cada tanque

--- src/edu/unlam/estanque/ArchivoEstanque.java
+++ src/edu/unlam/estanque/ArchivoEstanque.java
@@ -6,21 +6,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArchivoEstanque {
-	
-	public static ConjuntoEstanque leer(String path) throws FileNotFoundException{
+
+	public static ConjuntoEstanque leer(String path) throws FileNotFoundException {
 		ArrayList<Estanque> estanques = new ArrayList<>();
 		Scanner lector = new Scanner(new File(path));
 		int cantEstanques = lector.nextInt();
 		int volumenDeAgua;
-		
-		for(int i=0; i<cantEstanques-1;i++) 
-			estanques.add(new Estanque(i+1,lector.nextInt(), lector.nextInt(), lector.nextInt()));
-		
-		//ultimo estanque sin caño dererecho
-		estanques.add(new Estanque(cantEstanques, lector.nextInt(), lector.nextInt(), 0));
+		int profundidadCañoAnterior=0;
+
+		estanques.add(new Estanque(1, lector.nextInt(), lector.nextInt(), profundidadCañoAnterior,
+				profundidadCañoAnterior = lector.nextInt()));
+
+		for (int i = 1; i < cantEstanques - 1; i++)
+			estanques.add(new Estanque(i + 1, lector.nextInt(), lector.nextInt(), profundidadCañoAnterior,
+					profundidadCañoAnterior = lector.nextInt()));
+
+		// ultimo estanque sin caño dererecho
+		estanques.add(new Estanque(cantEstanques, lector.nextInt(), lector.nextInt(), profundidadCañoAnterior,0));
 		volumenDeAgua = lector.nextInt();
 		lector.close();
-		
+
 		return new ConjuntoEstanque(estanques, cantEstanques, volumenDeAgua);
 	}
 
--- src/edu/unlam/estanque/Estanque.java
+++ src/edu/unlam/estanque/Estanque.java
@@ -4,23 +4,27 @@ public class Estanque {
 	int nroTanque;
 	int superficie;
 	int profundidad;
-	int profunidadadCaño;
+	int profundidadCañoIzq;
+	int profundidadCañoDer;
 	int volumenDisponible;
-	
-	public Estanque(int nroTanque, int superficie, int profundidad, int profunidadadCaño) {
+
+	public Estanque(int nroTanque, int superficie, int profundidad, int profundidadCañoIzq, int profundidadCañoDer) {
 		this.nroTanque = nroTanque;
 		this.superficie = superficie;
 		this.profundidad = profundidad;
-		this.profunidadadCaño = profunidadadCaño;
-		this.volumenDisponible = superficie*profundidad;
+		this.profundidadCañoIzq = profundidadCañoIzq;
+		this.profundidadCañoDer = profundidadCañoDer;
+		this.volumenDisponible = superficie * profundidad;
 	}
 
 	@Override
 	public String toString() {
-		return this.nroTanque + "  " + this.superficie + "  " + this.profundidad + "  " + this.profunidadadCaño;
+		return this.nroTanque + "  " + this.superficie + "  " + this.profundidad + "  " + this.profundidadCañoIzq + " "
+				+ this.profundidadCañoDer;
 	}
-	
-	
-	
+
+	public void llenar() {
+		// Primero lleno el tanque vacio, pero esto depende de la posición de sus caños.
+	}
 
 }
